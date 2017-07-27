bae708ff01d5d797193d957a1f1c0a1abc9b11f6
huangyunfeng <283563375@qq.com>
2017-07-27 08:56:52 +0800

    react component

--- react-pic-anmi/src/js/containers/Home.js
+++ react-pic-anmi/src/js/containers/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Img from '../components/Img'
+import Loading from '../components/Loading'
 import defaultImg from '../../imgs/cnodejs.png'
  
 class Home extends React.Component {
   render() {
     return (
+      <div>
       <Img  imageUrl="http://img0.imgtn.bdimg.com/it/u=783sdfgdfgdfsgd060973,4278100629&fm=26&gp=0adsfsdfge.jpg"
             defaultImg={defaultImg}
             w='5rem'
             h='4rem'
       />  
+      <Loading r='50' z='4' c='red'/>  
+      </div>
     );
   }
 }
